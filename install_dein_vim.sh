066663bca4e256abab5a918ae44d67f509fd5807
pochy <kenta.teruteru@gmail.com>
2018-05-28 21:32:29 +0900

    fix

--- install_dein_vim.sh
+++ install_dein_vim.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd ~/dotfiles/.vim
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-sh ./installer.sh ~/dotfiles/.vim/bundles
+sh ./installer.sh ~/dotfiles/.vim/bundle
 rm installer.sh
