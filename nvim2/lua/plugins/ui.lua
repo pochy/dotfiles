59ac3704cf4de031683f6c940509372b41137b6d
knakajima <kenta.nakajima@psp.co.jp>
2024-04-25 05:15:13 +0900

    update neovim

--- nvim2/lua/plugins/ui.lua
+++ nvim2/lua/plugins/ui.lua
@@ -50,6 +50,9 @@ return {
     "echasnovski/mini.animate",
     event = "VeryLazy",
     opts = function(_, opts)
+      opts.cursor = {
+        enable = false,
+      }
       opts.scroll = {
         enable = false,
       }
