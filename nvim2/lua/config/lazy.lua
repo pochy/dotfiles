2bf1aa0697fe675df5e8f9f39348f3b5b560a15e
knakajima <kenta.teruteru@gmail.com>
2024-05-18 13:31:02 +0900

    add dap, test

--- nvim2/lua/config/lazy.lua
+++ nvim2/lua/config/lazy.lua
@@ -19,6 +19,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.test.core" },
+    { import = "lazyvim.plugins.extras.dap.core" },
     -- import/override with your plugins
     { import = "plugins" },
   },
