7bea56cc36089b8172f38ab3e66dd9ef3a0b6002
pochy <kenta.teruteru@gmail.com>
2026-05-13 08:53:00 +0900

    Format lualine status components

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -67,24 +67,44 @@ return {
         lualine_x = {
           Snacks.profiler.status(),
           {
-            function() return require("noice").api.status.command.get() end,
-            cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
-            color = function() return { fg = Snacks.util.color("Statement") } end,
+            function()
+              return require("noice").api.status.command.get()
+            end,
+            cond = function()
+              return package.loaded["noice"] and require("noice").api.status.command.has()
+            end,
+            color = function()
+              return { fg = Snacks.util.color("Statement") }
+            end,
           },
           {
-            function() return require("noice").api.status.mode.get() end,
-            cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-            color = function() return { fg = Snacks.util.color("Constant") } end,
+            function()
+              return require("noice").api.status.mode.get()
+            end,
+            cond = function()
+              return package.loaded["noice"] and require("noice").api.status.mode.has()
+            end,
+            color = function()
+              return { fg = Snacks.util.color("Constant") }
+            end,
           },
           {
-            function() return "  " .. require("dap").status() end,
-            cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
-            color = function() return { fg = Snacks.util.color("Debug") } end,
+            function()
+              return "  " .. require("dap").status()
+            end,
+            cond = function()
+              return package.loaded["dap"] and require("dap").status() ~= ""
+            end,
+            color = function()
+              return { fg = Snacks.util.color("Debug") }
+            end,
           },
           {
             require("lazy.status").updates,
             cond = require("lazy.status").has_updates,
-            color = function() return { fg = Snacks.util.color("Special") } end,
+            color = function()
+              return { fg = Snacks.util.color("Special") }
+            end,
           },
           {
             "diff",
@@ -112,7 +132,9 @@ return {
         lualine_z = {
           -- 最後に丸みを持たせる
           {
-            function() return " " .. os.date("%R") end,
+            function()
+              return " " .. os.date("%R")
+            end,
             separator = { right = "" },
             left_padding = 2,
           },
